4507b8a00f25c6a28511f576dc31b3351614e513
gundralaa <gundralaa@berkeley.edu>
2022-05-06 17:56:18 -0700

    new romi template stub on fork

--- platform/platform.h
+++ platform/platform.h
@@ -2,17 +2,13 @@
 
 /*************
 Copyright (c) 2021, The University of California at Berkeley.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
-
 1. Redistributions of source code must retain the above copyright notice,
    this list of conditions and the following disclaimer.
-
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
    and/or other materials provided with the distribution.
-
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
 EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
 MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
@@ -42,7 +38,6 @@ THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
-
 // nRF52832
 #include "platform/lf_nRF52832_support.h"
 
@@ -71,6 +66,11 @@ typedef _interval_t interval_t;
 typedef _microstep_t microstep_t;
 
 #ifdef NUMBER_OF_WORKERS
+
+/**
+ * @brief Get the number of cores on the host machine.
+ */
+extern int lf_available_cores();
 
 /**
  * Create a new thread, starting with execution of lf_thread
@@ -151,12 +151,59 @@ extern int lf_cond_wait(lf_cond_t* cond, lf_mutex_t* mutex);
  */
 extern int lf_cond_timedwait(lf_cond_t* cond, lf_mutex_t* mutex, instant_t absolute_time_ns);
 
+/*
+ * Atomically increment the variable that ptr points to by the given value, and return the original value of the variable.
+ * @param ptr A pointer to a variable. The value of this variable will be replaced with the result of the operation.
+ * @param value The value to be added to the variable pointed to by the ptr parameter.
+ * @return The original value of the variable that ptr points to (i.e., from before the application of this operation).
+ */
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+// Assume that an integer is 32 bits.
+#define lf_atomic_fetch_add(ptr, value) InterlockedExchangeAdd(ptr, value)
+#elif defined(__GNUC__) || defined(__clang__)
+#define lf_atomic_fetch_add(ptr, value) __sync_fetch_and_add(ptr, value)
+#else
+#error "Compiler not supported"
+#endif
+
+/*
+ * Atomically increment the variable that ptr points to by the given value, and return the new value of the variable.
+ * @param ptr A pointer to a variable. The value of this variable will be replaced with the result of the operation.
+ * @param value The value to be added to the variable pointed to by the ptr parameter.
+ * @return The new value of the variable that ptr points to (i.e., from before the application of this operation).
+ */
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+// Assume that an integer is 32 bits.
+#define lf_atomic_add_fetch(ptr, value) InterlockedAdd(ptr, value)
+#elif defined(__GNUC__) || defined(__clang__)
+#define lf_atomic_add_fetch(ptr, value) __sync_add_and_fetch(ptr, value)
+#else
+#error "Compiler not supported"
+#endif
+
+/*
+ * Atomically compare the variable that ptr points to against oldval. If the
+ * current value is oldval, then write newval into *ptr.
+ * @param ptr A pointer to a variable.
+ * @param oldval The value to compare against.
+ * @param newval The value to assign to *ptr if comparison is successful.
+ * @return The true if comparison was successful. False otherwise.
+ */
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+// Assume that an integer is 32 bits.
+#define lf_bool_compare_and_swap(ptr, oldval, newval) (InterlockedCompareExchange(ptr, newval, oldval) == oldval)
+#elif defined(__GNUC__) || defined(__clang__)
+#define lf_bool_compare_and_swap(ptr, oldval, newval) __sync_bool_compare_and_swap(ptr, oldval, newval)
+#else
+#error "Compiler not supported"
+#endif
+
 #endif
 
 /**
  * Initialize the LF clock. Must be called before using other clock-related APIs.
  */
-extern void lf_initialize_clock();
+extern void lf_initialize_clock(void);
 
 /**
  * Fetch the value of an internal (and platform-specific) physical clock and 
@@ -175,5 +222,17 @@ extern int lf_clock_gettime(instant_t* t);
  * @return 0 for success, or -1 for failure.
  */
 extern int lf_nanosleep(instant_t requested_time);
+
+
+/**
+ * Macros for marking function as deprecated
+ */
+#ifdef __GNUC__
+#define DEPRECATED(X) X __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define DEPRECATED(X) __declspec(deprecated) X
+#else
+#define DEPRECATED(X) X
+#endif
 
 #endif // PLATFORM_H
---
+++ src/RomiTemplate/states.h
@@ -0,0 +1,6 @@
+// define more states
+// used across all reactors
+typedef enum {
+  OFF,
+  DRIVING,
+} robot_state_t;
