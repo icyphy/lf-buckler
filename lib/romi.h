9931e1a1f828c1aec21b6ef1fe6bc4aeecabe676
Shaokai Lin <shaokai_lin@126.com>
2022-09-22 17:33:32 -0700

    Fix spacing inconsistencies by applying C formatter

--- lib/romi.h
+++ lib/romi.h
@@ -18,25 +18,28 @@
 /**
  * @brief Bumpers.
  */
-typedef struct {
+typedef struct
+{
     // Indicates if bumper is currently pressed
-	bool	left;   // Or of two left bump sensors.
-	bool	center; // Or of two center bump sensors.
-	bool	right;  // Or of two right bump sensors.
+    bool left;   // Or of two left bump sensors.
+    bool center; // Or of two center bump sensors.
+    bool right;  // Or of two right bump sensors.
 } romi_bumps_t;
 
 /**
  * @brief Buttons.
  */
-typedef struct {
-	bool	left;
-	bool	right;
+typedef struct
+{
+    bool left;
+    bool right;
 } romi_buttons_t;
 
 /**
  * @brief Reflectance, which is 0 for light color, 1 for dark color.
  */
-typedef struct {
+typedef struct
+{
     bool left;
     bool center;
     bool right;
@@ -45,7 +48,8 @@ typedef struct {
 /**
  * @brief Motor encoder data.
  */
-typedef struct {
+typedef struct
+{
     // 16 bit unsigned roll over. Forward is positive.
     uint16_t left;
     uint16_t right;
@@ -54,21 +58,22 @@ typedef struct {
 /**
  * @brief Collection of sensor data from the Romi.
  */
-typedef struct{
+typedef struct
+{
     uint16_t time_stamp;
 
-	// Bump sensors.
-	romi_bumps_t bumps;
+    // Bump sensors.
+    romi_bumps_t bumps;
 
-	// Buttons.
-	romi_buttons_t buttons;
+    // Buttons.
+    romi_buttons_t buttons;
 
     // Reflectance sensor.
     romi_reflectance_t reflectance;
 
     // Wheel encoders.
     romi_encoder_t encoders;
-    
+
 } romi_sensors_t;
 
 //////////////////////////////////////////////////////////////
@@ -79,18 +84,18 @@ typedef struct{
  * This function compares the state of the buttons to what they were on
  * the previous call to this function, and if either button is now pressed and
  * was not previously pressed, this returns true.
- * 
+ *
  * @param sensors The sensor struct for the Romi.
  * @return true If a button has been pressed.
  */
-bool romi_button_pressed(romi_sensors_t* const sensors);
+bool romi_button_pressed(romi_sensors_t *const sensors);
 
 /**
  * @brief Set the speed of the left and right wheels.
  * The speed is in units of mm/s.
  * A good starting point for experimentations is 75 mm/s.
  * This function assumes that romi_init() has been called.
- * 
+ *
  * @param left_wheel_speed The left wheel speed in mm/s.
  * @param right_wheel_speed The right wheel speed in mm/s.
  * @return int32_t An error code that should be checked using the macro APP_ERROR_CHECK.
@@ -105,10 +110,10 @@ uint32_t romi_init();
 
 /**
  * @brief Read the sensors from the Romi robot.
- * 
+ *
  * @param sensors The struct into which to write the sensor values.
  * @return int32_t An error code that should be checked using the macro APP_ERROR_CHECK.
  */
-int32_t romi_sensors_poll(romi_sensors_t* const	sensors);
+int32_t romi_sensors_poll(romi_sensors_t *const sensors);
 
 #endif
