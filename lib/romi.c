c46f9bb19d3a7003593635ffbcee30d2ff16ebc3
Edward A. Lee <eal@eecs.berkeley.edu>
2022-09-22 17:38:06 -0700

    Create lib folder with Romi-specific support (#17)
    * Comment out call to nrf_gpio_pin_clear(PIN1), which does not compile
    
    * Attempts to get UART to work with printf
    
    * Comments only
    
    * Use low-level UART API instead.
    Add some error handling in case of buffer overrun.
    
    * Move all UART stuff into romi.c.
    
    * Code cleanup and comments
    
    * Purged all Kobuki legacy and updated sensors struct to properly reflect Romi sensors
    
    * Moved several obsolete files to obsolete directory
    
    * Fix spacing inconsistencies by applying C formatter
    
    Co-authored-by: Erling Jellum <erling.r.jellum@ntnu.no>
    Co-authored-by: Shaokai Lin <shaokai@berkeley.edu>

---
+++ lib/romi.c
@@ -0,0 +1,452 @@
+/**
+ * @file romi.c
+ * @author Edward A. Lee
+ * @author Erling Jellum
+ * @brief Implementation of functions for controlling a TI-RLSK Romi robot.
+ * @version 0.1
+ *
+ * This implementation assumes the Romi robot has an nRF52 with a Berkeley
+ * Buckler board attached to its serial port, and that the Romi robot has
+ * been flashed to emulate the Kobuki robot protocol
+ * (see FIXME: pointer to repo for that firmware).
+ *
+ * This code is based on code in the buckler repo orginally developed by
+ * Jeff C. Jensen, Joshua Adkins, and Neal Jackson.
+ */
+#include "lib/romi.h"
+#include "buckler.h"
+#include <math.h>
+#include <string.h> // Defines memcpy
+#include <stdlib.h> // Defines abs
+#include <stdint.h> // Defines uint8_t, etc.
+
+// nRF library files.
+#include "nrf_drv_clock.h" // Defines nrf_drv_clock_init() and nrf_drv_clock_lfclk_request().
+#include "nrfx_uart.h"
+#include "app_timer.h" // Defines app_timer_init()
+
+// See romi.h for function documentation.
+
+// NOTE: The implementation here uses code in the buckler repo.
+// Eventually, it should replace all that code.
+
+///////////////////////////////////////////////////////////////////////////////////////
+//// Private functions. Not documented in romi.h. Used in public functions below.
+//// Not intended to be called by users.
+
+// UART implementation
+static nrfx_uart_t nrfx_uart = NRFX_UART_INSTANCE(0);
+static nrfx_uart_config_t nrfx_uart_cfg = NRFX_UART_DEFAULT_CONFIG;
+
+/**
+ * @brief Combine bytes into a 16 bit unsigned integer.
+ * @param low Low-order byte.
+ * @param high High-order byte.
+ * @return The combined bytes.
+ */
+static uint16_t to_uint16(uint8_t low, uint16_t high)
+{
+    return ((uint16_t)high << 8) | low;
+}
+
+/**
+ * @brief Calculate and return a checksum for the specified buffer.
+ * This is based on checkSum() in the buckler repo.
+ *
+ * @param buffer The buffer.
+ * @param length The length of the buffer.
+ * @return The XOR of the bytes in the buffer.
+ */
+static uint8_t _romi_checksum(uint8_t *buffer, int length)
+{
+    uint8_t cs = 0x00;
+    // printf("\n l = %d \n",length);
+    for (int i = 2; i < length; i++)
+    {
+        // printf("%d ",buffer[i]);
+        cs ^= buffer[i];
+    }
+    // printf("\n last[%d]=%d cs=%d",i,buffer[i],cs);
+    return cs;
+}
+
+/**
+ * @brief Send the specified payload over UART to the Romi robot.
+ * This function adds a header (0xAA and 0x55) and a length to
+ * the front of the byte stream that is sent, plus a checksum at
+ * the end.
+ * This is based on kobukiSendPayload from the buckler repo,
+ * converted to use lower-level UART functions.
+ *
+ * @param payload The payload.
+ * @param length The size of the payload.
+ * @return An error code that should be checked using the macro APP_ERROR_CHECK.
+ */
+static int32_t _romi_send_payload(uint8_t *payload, uint8_t length)
+{
+    uint8_t writeData[256] = {0};
+
+    // Write move payload
+    writeData[0] = 0xAA;
+    writeData[1] = 0x55;
+    writeData[2] = length;
+    memcpy(writeData + 3, payload, length);
+    writeData[3 + length] = _romi_checksum(writeData, 3 + length);
+
+    return nrfx_uart_tx(&nrfx_uart, writeData, length + 4);
+}
+
+/**
+ * @brief Send a drive command with specified radius and speed to the Romi.
+ * This is based on kobukiDriveRadius from the buckler repo.
+ *
+ * @param radius The radius.
+ * @param speed The speed.
+ * @return An error code that should be checked using the macro APP_ERROR_CHECK.
+ */
+static int32_t _romi_drive_radius(int16_t radius, int16_t speed)
+{
+    uint8_t payload[6];
+    payload[0] = 0x01;
+    payload[1] = 0x04;
+    memcpy(payload + 2, &speed, 2);
+    memcpy(payload + 4, &radius, 2);
+
+    return _romi_send_payload(payload, 6);
+}
+
+/**
+ * @brief Initialize the UART to a baud rate of 115,200.
+ * @return An error code that should be checked using the macro APP_ERROR_CHECK.
+ */
+static int32_t _romi_uart_init()
+{
+    nrfx_uart_cfg.pseltxd = BUCKLER_UART_TX;
+    nrfx_uart_cfg.pselrxd = BUCKLER_UART_RX;
+    nrfx_uart_cfg.parity = NRF_UART_PARITY_EXCLUDED;
+    nrfx_uart_cfg.baudrate = NRF_UART_BAUDRATE_115200;
+    nrfx_uart_cfg.interrupt_priority = NRFX_UART_DEFAULT_CONFIG_IRQ_PRIORITY;
+
+    // Initialize UART.
+    return nrfx_uart_init(&nrfx_uart, &nrfx_uart_cfg, NULL);
+}
+
+/**
+ * @brief Read a packet from the serial port.
+ * Read from the serial port until a header AA55 is encountered.
+ * Then read the length of the packet.
+ * Then read the packet into the specified buffer.
+ *
+ * @param packetBuffer The buffer into which to put the result.
+ * @param len The length of the buffer.
+ * @return int32_t
+ */
+static int32_t _romi_read_serial(uint8_t *packetBuffer, uint8_t len)
+{
+    // States of the state machine depending on what is read next.
+    // Header should start with AA55.
+    typedef enum
+    {
+        wait_until_header,
+        read_length,
+        read_payload,
+        read_checksum
+    } state_type;
+
+    state_type state = wait_until_header;
+    uint8_t header_buf[2];
+    uint8_t payloadSize = 0;
+    uint8_t calcuatedCS = 0;
+    uint8_t byteBuffer = 0;
+    nrfx_err_t ret;
+    int err_cnt = 0;
+    bool done = false;
+    // Enable reception on UART
+    nrfx_uart_rx_enable(&nrfx_uart);
+
+    int num_checksum_failures = 0;
+
+    if (len <= 4)
+    {
+        ret = NRF_ERROR_NO_MEM;
+        done = true;
+    }
+
+    while (!done)
+    {
+        switch (state)
+        {
+        case wait_until_header:
+            ret = nrfx_uart_rx(&nrfx_uart, header_buf, 2);
+            if (ret != NRF_SUCCESS)
+            {
+                nrfx_uart_errorsrc_get(&nrfx_uart);
+                err_cnt++;
+                break;
+            }
+
+            if (header_buf[0] == 0xAA && header_buf[1] == 0x55)
+            {
+                state = read_length;
+            }
+            else
+            {
+                state = wait_until_header;
+            }
+            break;
+
+        case read_length:
+            ret = nrfx_uart_rx(&nrfx_uart, &payloadSize, sizeof(payloadSize));
+            if (ret != NRF_SUCCESS)
+            {
+                nrfx_uart_errorsrc_get(&nrfx_uart);
+                err_cnt++;
+                state = wait_until_header;
+                break;
+            }
+            if (len < payloadSize + 3)
+            {
+                done = true;
+                ret = NRF_ERROR_NO_MEM;
+            }
+            state = read_payload;
+            break;
+
+        case read_payload:
+            ret = nrfx_uart_rx(&nrfx_uart, packetBuffer + 3, payloadSize + 1);
+            if (ret != NRF_SUCCESS)
+            {
+                err_cnt++;
+                nrfx_uart_errorsrc_get(&nrfx_uart);
+                state = wait_until_header;
+                break;
+            }
+            state = read_checksum;
+            break;
+
+        case read_checksum:
+            memcpy(packetBuffer, header_buf, 2);
+            packetBuffer[2] = payloadSize;
+
+            calcuatedCS = _romi_checksum(packetBuffer, payloadSize + 3);
+            byteBuffer = (packetBuffer)[payloadSize + 3];
+            done = true;
+            if (calcuatedCS == byteBuffer)
+            {
+                num_checksum_failures = 0;
+                ret = NRF_SUCCESS;
+            }
+            else
+            {
+                state = wait_until_header;
+                if (num_checksum_failures == 3)
+                {
+                    ret = -1500;
+                }
+                num_checksum_failures++;
+            }
+            break;
+
+        default:
+            break;
+        }
+        // If we get too many errors abort the sensor reading.
+        if (ret != NRF_SUCCESS && err_cnt >= 20)
+        {
+            done = true;
+        }
+    }
+    nrfx_uart_rx_disable(&nrfx_uart);
+
+    return ret;
+}
+
+/**
+ * @brief Parse the sensor data sent over the UART by the robot.
+ * This is based on kobukiParseSensorPacket in kobukiSensors.c in the buckler repo.
+ *
+ * @param packet The raw sensor data.
+ * @param sensors The struct into which to insert the data.
+ */
+static void _romi_parse_sensor_packet(const uint8_t *packet, romi_sensors_t *sensors)
+{
+    uint8_t payload_length = packet[2];
+    uint8_t subpayload_length = 0;
+
+    uint8_t i = 3;
+    while (i < payload_length + 3)
+    {
+
+        uint8_t id = packet[i];
+
+        subpayload_length = packet[i + 1];
+
+        switch (id)
+        {
+        case 0x01:
+            // There's an ambiguity in the documentation where
+            // it says there are two headers with value 0x01:
+            // basic sensor data and controller info - although it
+            // says elsewhere that controller info has ID 0x15
+            // so we'll just check here to make sure it's the right length
+
+            if (subpayload_length == 0x0F)
+            {
+                sensors->time_stamp = to_uint16(packet[i + 2], packet[i + 3]);
+
+                sensors->bumps.right = packet[i + 4] & 0x01;
+                sensors->bumps.center = (packet[i + 4] & 0x02);
+                sensors->bumps.left = (packet[i + 4] & 0x04);
+
+                sensors->reflectance.right = (packet[i + 6] & 0x01);
+                sensors->reflectance.center = (packet[i + 6] & 0x02);
+                sensors->reflectance.left = (packet[i + 6] & 0x04);
+
+                sensors->encoders.left = to_uint16(packet[i + 7], packet[i + 8]);
+                sensors->encoders.right = to_uint16(packet[i + 9], packet[i + 10]);
+
+                sensors->buttons.right = (bool)(packet[i + 13] & 0x01);
+                sensors->buttons.left = (bool)(packet[i + 13] & 0x02);
+
+                i += subpayload_length + 2; // + 2 for header and length
+            }
+            else
+            {
+                i += payload_length + 3; // add enough to terminate the outer while loop
+            }
+
+            break;
+
+        default:
+            printf("Unexpected message type over UART: %d", id);
+            break;
+        }
+    }
+    // Checksum has already been checked.
+    return;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+//// Public functions. Documented in romi.h. Intended to be called by users.
+
+bool romi_button_pressed(romi_sensors_t *const sensors)
+{
+    // save previous states of buttons
+    static bool previous_left = false;
+    static bool previous_right = false;
+
+    bool result = false;
+
+    // Check left button.
+    bool current_left = sensors->buttons.left;
+    if (current_left && previous_left != current_left)
+    {
+        result = true;
+    }
+    previous_left = current_left;
+
+    // check right
+    bool current_right = sensors->buttons.right;
+    if (current_right && previous_right != current_right)
+    {
+        result = true;
+    }
+    previous_right = current_right;
+
+    return result;
+}
+
+// Based on kobukiDriveDirect from the buckler repo, but with a number of fixes.
+int32_t romi_drive_direct(int16_t left_wheel_speed, int16_t right_wheel_speed)
+{
+    // Convert the independent wheel speeds into an approximate radius and speed.
+    int32_t speed;
+    int32_t radius;
+    int32_t left_right;
+
+    // Choose the larger of the two speeds.
+    if (abs(right_wheel_speed) > abs(left_wheel_speed))
+    {
+        speed = right_wheel_speed;
+        left_right = 1;
+    }
+    else
+    {
+        speed = left_wheel_speed;
+        left_right = -1;
+    }
+
+    if (right_wheel_speed == left_wheel_speed)
+    {
+        radius = 0; // Special case 0 commands Kobuki travel with infinite radius.
+    }
+    else
+    {
+        double estimate = (right_wheel_speed + left_wheel_speed) / (2.0 * (right_wheel_speed - right_wheel_speed) / 123.0);
+        // The value 123 was determined experimentally to work, and is approximately 1/2 the wheelbase in mm.
+
+        radius = round(estimate);
+        if (radius == 0)
+            radius = left_right; // Avoid special case 0 unless want infinite radius.
+        // if the above statement overflows a signed 16 bit value, set radius=0 for infinite radius.
+        if (radius > 32767)
+            radius = 0;
+        if (radius < -32768)
+            radius = 0;
+    }
+    /* Original had the following rather odd adjustment with no comment.
+    if (radius == 1){
+        speed = speed * -1;
+    }
+    */
+
+    return _romi_drive_radius(radius, speed);
+}
+
+uint32_t romi_init()
+{
+    uint32_t err_code = nrf_drv_clock_init();
+    if (err_code != NRF_ERROR_MODULE_ALREADY_INITIALIZED)
+    {
+        APP_ERROR_CHECK(err_code);
+        err_code = NRF_SUCCESS; // This is OK.
+    }
+
+    nrf_drv_clock_lfclk_request(NULL);
+    err_code = app_timer_init();
+    if (err_code != NRF_ERROR_MODULE_ALREADY_INITIALIZED)
+    {
+        APP_ERROR_CHECK(err_code);
+        err_code = NRF_SUCCESS; // This is OK.
+    }
+
+    err_code = _romi_uart_init();
+    if (err_code != NRF_SUCCESS)
+    {
+        APP_ERROR_CHECK(err_code);
+        return err_code;
+    }
+
+    // Make sure the robot is stopped.
+    err_code = romi_drive_direct(0, 0);
+    APP_ERROR_CHECK(err_code);
+
+    return err_code;
+}
+
+int32_t romi_sensors_poll(romi_sensors_t *const sensors)
+{
+    // initialize communications buffer
+    // We know that the maximum size of the packet is less than 140 based on documentation
+    uint8_t packet[140] = {0};
+    int status = _romi_read_serial(packet, 140);
+    if (status != NRF_SUCCESS)
+    {
+        return status;
+    }
+
+    // parse response
+    _romi_parse_sensor_packet(packet, sensors);
+
+    return status;
+}
---
+++ lib/romi.h
@@ -0,0 +1,119 @@
+/**
+ * @file romi.h
+ * @author Edward A. Lee
+ * @brief Header file for functions for controlling a TI-RLSK Romi robot.
+ * @version 0.1
+ */
+#ifndef ROMI_H
+#define ROMI_H
+
+// To define APP_ERROR_CHECK, a macro that flashes the lights on a robot when
+// something goes wrong.
+#include "app_error.h"
+#include <stdint.h> // Defines uint8_t, etc.
+
+//////////////////////////////////////////////////////////////
+//// Data structures
+
+/**
+ * @brief Bumpers.
+ */
+typedef struct
+{
+    // Indicates if bumper is currently pressed
+    bool left;   // Or of two left bump sensors.
+    bool center; // Or of two center bump sensors.
+    bool right;  // Or of two right bump sensors.
+} romi_bumps_t;
+
+/**
+ * @brief Buttons.
+ */
+typedef struct
+{
+    bool left;
+    bool right;
+} romi_buttons_t;
+
+/**
+ * @brief Reflectance, which is 0 for light color, 1 for dark color.
+ */
+typedef struct
+{
+    bool left;
+    bool center;
+    bool right;
+} romi_reflectance_t;
+
+/**
+ * @brief Motor encoder data.
+ */
+typedef struct
+{
+    // 16 bit unsigned roll over. Forward is positive.
+    uint16_t left;
+    uint16_t right;
+} romi_encoder_t;
+
+/**
+ * @brief Collection of sensor data from the Romi.
+ */
+typedef struct
+{
+    uint16_t time_stamp;
+
+    // Bump sensors.
+    romi_bumps_t bumps;
+
+    // Buttons.
+    romi_buttons_t buttons;
+
+    // Reflectance sensor.
+    romi_reflectance_t reflectance;
+
+    // Wheel encoders.
+    romi_encoder_t encoders;
+
+} romi_sensors_t;
+
+//////////////////////////////////////////////////////////////
+//// Functions
+
+/**
+ * @brief Return true if the left or right button changes from not pressed to pressed.
+ * This function compares the state of the buttons to what they were on
+ * the previous call to this function, and if either button is now pressed and
+ * was not previously pressed, this returns true.
+ *
+ * @param sensors The sensor struct for the Romi.
+ * @return true If a button has been pressed.
+ */
+bool romi_button_pressed(romi_sensors_t *const sensors);
+
+/**
+ * @brief Set the speed of the left and right wheels.
+ * The speed is in units of mm/s.
+ * A good starting point for experimentations is 75 mm/s.
+ * This function assumes that romi_init() has been called.
+ *
+ * @param left_wheel_speed The left wheel speed in mm/s.
+ * @param right_wheel_speed The right wheel speed in mm/s.
+ * @return int32_t An error code that should be checked using the macro APP_ERROR_CHECK.
+ */
+int32_t romi_drive_direct(int16_t leftWheelSpeed, int16_t rightWheelSpeed);
+
+/**
+ * @brief Initialize the Romi robot.
+ * @return int32_t An error code that should be checked using the macro APP_ERROR_CHECK.
+ */
+uint32_t romi_init();
+
+/**
+ * @brief Read the sensors from the Romi robot.
+ *
+ * @param sensors The struct into which to write the sensor values.
+ * @return int32_t An error code that should be checked using the macro APP_ERROR_CHECK.
+ */
+int32_t romi_sensors_poll(romi_sensors_t *const sensors);
+
+#endif
