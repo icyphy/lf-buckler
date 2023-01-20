c7a10ca181124d6b3592a7eb62e8aea31dca66e6
Abhinav Gundrala <gundralaa@Abhinavs-MacBook-Pro.local>
2023-01-20 12:22:08 -0800

    mac support

--- test/scripts/test_prolog.sh
+++ test/scripts/test_prolog.sh
@@ -1,5 +1,9 @@
 # Start JLink server
-echo STARTING JLINK SERVER
-x-terminal-emulator -e "JLinkExe -device nrf52832_xxaa -if swd -speed 4000 -AutoConnect 1"
+echo STARTING JLINK SERVER  
+case $( uname -s ) in 
+Darwin) osascript -e 'tell application "Terminal" to do script "JLinkExe -device nrf52832_xxaa -if swd -speed 4000 -AutoConnect 1"';; 
+*) x-terminal-emulator -e "JLinkExe -device nrf52832_xxaa -if swd -speed 4000 -AutoConnect 1";;
+esac
+
 echo STARTING JLINK RTT CLIENT
 JLinkRTTClient > $1 &
